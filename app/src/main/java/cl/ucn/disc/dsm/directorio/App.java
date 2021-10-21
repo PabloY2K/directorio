fd8354190477b58754ddefad4e74d4bc6c9a3b69
PabloY2K <pablo.herrera@alumnos.ucn.cl>
2021-10-21 13:05:20 -0300

    cambios de la clase del 21/10/21

--- app/src/main/java/cl/ucn/disc/dsm/directorio/App.java
+++ app/src/main/java/cl/ucn/disc/dsm/directorio/App.java
@@ -2,11 +2,13 @@ package cl.ucn.disc.dsm.directorio;
 
 import android.app.Application;
 import android.content.Context;
-
 import org.acra.ACRA;
-import org.acra.annotation.AcraCore;
+import org.acra.config.CoreConfigurationBuilder;
+import org.acra.config.DialogConfigurationBuilder;
+import org.acra.config.MailSenderConfigurationBuilder;
+import org.acra.data.StringFormat;
 
-@AcraCore(buildConfigClass = BuildConfig.class)
+
 
 public class App extends Application {
 
@@ -15,7 +17,30 @@ public class App extends Application {
     protected void attachBaseContext(Context base) {
         super.attachBaseContext(base);
 
+        CoreConfigurationBuilder builder = new CoreConfigurationBuilder(this);
+        builder
+                .withBuildConfigClass(BuildConfig.class)
+                .withReportFormat(StringFormat.JSON)
+                .withEnabled(true);
+
+        //ACRA Dialog Configuration
+        builder.getPluginConfigurationBuilder(DialogConfigurationBuilder.class)
+                //required
+                .withResText(R.string.acra_dialog_title)
+                .withResCommentPrompt(R.string.acra_dialog_comment)
+                .withEnabled(true);
+
+        //ACRA Email Configuration
+        builder.getPluginConfigurationBuilder(MailSenderConfigurationBuilder.class)
+                .withMailTo("pablo.herrera@alumnos.ucn.cl")
+                .withReportFileName("crash.txt")
+                .withSubject(getString(R.string.acra_dialog_title))
+                .withBody(getString(R.string.acra_dialog_comment))
+                .withEnabled(true);
+
+
+
         //The following Line triggers the initialization of ACRA
-        ACRA.init(this);
+        ACRA.init(this, builder);
     }
 }
